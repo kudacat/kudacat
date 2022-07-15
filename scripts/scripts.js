c02905d7d847e12adf504ee16debb93f584626ab
sam-flores <109257620+kudako-io@users.noreply.github.com>
2022-07-14 21:42:17 -0700

    lji

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,89 +1,32 @@
-	var moreLyrics = document.getElementById("moreLyrics");
-	var moreChron = document.getElementById("moreChron");
-	var morePReel = document.getElementById("morePReel");
-	var moreFront = document.getElementById("moreFront");
-	var moreBack = document.getElementById("moreBack");
-	var myBackBtn = document.getElementById("myBackBtn");
-	var myFrontBtn = document.getElementById("myFrontBtn");
-	var myLyricBtn = document.getElementById("myLyricBtn");
-	var myPReelBtn = document.getElementById("myPReelBtn");
-	const el = document.getElementById('myNavBtns');
-	var scale;
+window.onload = function() {
 
-	function frontTime() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-		if (moreFront.style.display === "inline") {
-			// moreFront.style.display = "none";
-		} else {
-			myFrontBtn.focus();
-			myLyricBtn.blur();
-			myBackBtn.blur();
-			myPReelBtn.blur();
-			moreFront.style.display = "inline";
-			moreLyrics.style.display = "none";
-			moreBack.style.display = "none";
-			morePReel.style.display = "none";
-		}
-	}
+"strict";
 
-	function backTime() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-		if (moreBack.style.display === "inline") {
-			// moreFront.style.display = "none";
-		} else {
-			myBackBtn.focus();
-			myFrontBtn.blur();
-			myLyricBtn.blur();
-			myPReelBtn.blur();
-			moreBack.style.display = "inline";
-			moreLyrics.style.display = "none";
-			moreFront.style.display = "none";
-			morePReel.style.display = "none";
-		}
-	}
+	$("#set_button").click(function(){
 
-	function lyricTime() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-		if (moreLyrics.style.display === "inline") {
-			// moreFront.style.display = "none";
-		} else {
-			myLyricBtn.focus();
-			myFrontBtn.blur();
-			myBackBtn.blur();
-			myPReelBtn.blur();
-			moreLyrics.style.display = "inline";
-			moreFront.style.display = "none";
-			moreBack.style.display = "none";
-			morePReel.style.display = "none";
+		if($("#dj_set").is(":visible"))
+		{
+			$("#dj_set").hide();
+			$(".right-button").animate({top: '50vh'});
+			$(".left-button").animate({top: '50vh'});
+		}else{
+			if($("#music").is(":visible")) $("#music").hide();
+			$("#dj_set").show();
+			$(".right-button").animate({bottom: '0vh'});
+			$(".left-button").animate({top: '0vh'});
 		}
-	}
-
-	function pReelTime() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-		if (morePReel.style.display === "inline") {
-			// moreFront.style.display = "none";
-		} else {
-			myPReelBtn.focus();
-			myFrontBtn.blur();
-			myBackBtn.blur();
-			myLyricBtn.blur();
-			morePReel.style.display = "inline";
-			moreLyrics.style.display = "none";
-			moreBack.style.display = "none";
-			moreFront.style.display = "none";
-		}
-	}
-	var prevScrollpos = window.pageYOffset;
-	window.onscroll = function() {
-		var currentScrollPos = window.pageYOffset;
-		if (prevScrollpos > currentScrollPos || window.pageYOffset < 20) {
-			document.getElementById("myNavBtns").style.top = "0";
-		} else {
-			document.getElementById("myNavBtns").style.top = "-20vh";
-		}
-		prevScrollpos = currentScrollPos;
-	}
+	});
+	// $("#music_button").click(function(){
+	// 	if($("#music").is(":visible"))
+	// 	{
+	// 		$("#music").hide();
+	// 		$(".left-button").animate({top: '50vh'});
+	// 		$(".right-button").animate({top: '50vh'});
+	// 	}else{
+	// 		$("#music").show();
+	// 		$(".right-button").animate({bottom: '0vh'});
+	// 		$(".left-button").animate({top: '0vh'});
+	// 		if($("#dj_set").is(":visible")) $("#dj_set").hide();
+	// 	}
+	// });
+};
