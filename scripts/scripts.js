2b4c1f8ee79bb7be85f343659c43f2da76b271bb
sam-flores <109257620+kudako-io@users.noreply.github.com>
2022-07-14 22:12:57 -0700

    ln

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,25 +7,25 @@ window.onload = function() {
 		if($("#dj_set").is(":visible"))
 		{
 			$("#dj_set").hide();
-			$(".right-button").animate({bottom: '50vh'});
-			$(".left-button").animate({bottom: '50vh'});
+			$(".right-button").animate({top: '60vh'});
+			$(".left-button").animate({top: '60vh'});
 		}else{
 			if($("#music").is(":visible")) $("#music").hide();
 			$("#dj_set").show();
-			$(".right-button").animate({bottom: '0vh'});
-			$(".left-button").animate({bottom: '0vh'});
+			$(".right-button").animate({top: '20vh'});
+			$(".left-button").animate({top: '20vh'});
 		}
 	});
 	$("#music_button").click(function(){
 		if($("#music").is(":visible"))
 		{
 			$("#music").hide();
-			$(".left-button").animate({bottom: '50vh'});
-			$(".right-button").animate({bottom: '50vh'});
+			$(".left-button").animate({top: '60vh'});
+			$(".right-button").animate({top: '60vh'});
 		}else{
 			$("#music").show();
-			$(".right-button").animate({bottom: '0vh'});
-			$(".left-button").animate({bottom: '0vh'});
+			$(".right-button").animate({top: '20vh'});
+			$(".left-button").animate({top: '20vh'});
 			if($("#dj_set").is(":visible")) $("#dj_set").hide();
 		}
 	});
