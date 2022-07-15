cefdea6ec951806a94af12f96002e2ee619f1f51
sam-flores <109257620+kudako-io@users.noreply.github.com>
2022-07-14 22:06:01 -0700

    jl

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,26 +7,26 @@ window.onload = function() {
 		if($("#dj_set").is(":visible"))
 		{
 			$("#dj_set").hide();
-			$(".right-button").animate({top: '50vh'});
-			$(".left-button").animate({top: '50vh'});
+			$(".right-button").animate({bottom: '50vh'});
+			$(".left-button").animate({bottom: '50vh'});
 		}else{
 			if($("#music").is(":visible")) $("#music").hide();
 			$("#dj_set").show();
 			$(".right-button").animate({bottom: '0vh'});
-			$(".left-button").animate({top: '0vh'});
+			$(".left-button").animate({bottom: '0vh'});
 		}
 	});
-	// $("#music_button").click(function(){
-	// 	if($("#music").is(":visible"))
-	// 	{
-	// 		$("#music").hide();
-	// 		$(".left-button").animate({top: '50vh'});
-	// 		$(".right-button").animate({top: '50vh'});
-	// 	}else{
-	// 		$("#music").show();
-	// 		$(".right-button").animate({bottom: '0vh'});
-	// 		$(".left-button").animate({top: '0vh'});
-	// 		if($("#dj_set").is(":visible")) $("#dj_set").hide();
-	// 	}
-	// });
+	$("#music_button").click(function(){
+		if($("#music").is(":visible"))
+		{
+			$("#music").hide();
+			$(".left-button").animate({bottom: '50vh'});
+			$(".right-button").animate({bottom: '50vh'});
+		}else{
+			$("#music").show();
+			$(".right-button").animate({bottom: '0vh'});
+			$(".left-button").animate({bottom: '0vh'});
+			if($("#dj_set").is(":visible")) $("#dj_set").hide();
+		}
+	});
 };
