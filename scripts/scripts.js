da2d0077370a3b60bd472d07986c75a5692b26dd
sam-flores <109257620+kudako-io@users.noreply.github.com>
2022-07-17 23:26:12 -0700

    fsd

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,6 +12,8 @@ window.onload = function() {
 			$("#soundcloud_iframe").attr("src", str);
 			if($("#music").is(":visible")){
 				$("#music").hide();
+			}else if($("#links").is(":visible")){
+				$("#links").hide();
 			}
 			$("#dj_set").show();
 		}
@@ -25,8 +27,23 @@ window.onload = function() {
 			$("#mixcloud_iframe").attr("src", str);
 			if($("#dj_set").is(":visible")){
 				$("#dj_set").hide();
+			}else if($("#links").is(":visible")){
+				$("#links").hide();
 			}
 			$("#music").show();
+		}
+	});
+	$("#links_button").click(function(){
+		if($("#links").is(":visible"))
+		{
+			$("#links").hide();
+		}else{
+			if($("#dj_set").is(":visible")){
+				$("#dj_set").hide();
+			}else if($("#music").is(":visible")){
+				$("#music").hide();
+			}
+			$("#links").show();
 		}
 	});
 };
