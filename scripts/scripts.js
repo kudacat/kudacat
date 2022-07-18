a1aeeab5c542c8aafd491d83c66d9ae49fa8950f
sam-flores <109257620+kudako-io@users.noreply.github.com>
2022-07-17 22:28:45 -0700

    fdsa

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,34 +7,26 @@ window.onload = function() {
 		if($("#dj_set").is(":visible"))
 		{
 			$("#dj_set").hide();
-			// $(".right-button").animate({top: '60vh'});
-			// $(".left-button").animate({top: '60vh'});
 		}else{
+			str = $("#soundcloud_iframe").attr("src");
+			$("#soundcloud_iframe").attr("src", str);
 			if($("#music").is(":visible")){
 				$("#music").hide();
-				str = $("#soundcloud_iframe").attr("src");
-				$("#soundcloud_iframe").attr("src", str);
-
 			}
 			$("#dj_set").show();
-			// $(".right-button").animate({top: '80vh'});
-			// $(".left-button").animate({top: '80vh'});
 		}
 	});
 	$("#music_button").click(function(){
 		if($("#music").is(":visible"))
 		{
 			$("#music").hide();
-			// $(".left-button").animate({top: '60vh'});
-			// $(".right-button").animate({top: '60vh'});
 		}else{
 			$("#music").show();
-			// $(".right-button").animate({top: '80vh'});
-			// $(".left-button").animate({top: '80vh'});
+			str = $("#mixcloud_iframe").attr("src");
+			$("#mixcloud_iframe").attr("src", str);
 			if($("#dj_set").is(":visible")){
-				str = $("#mixcloud_iframe").attr("src");
-				$("#mixcloud_iframe").attr("src", str);
-			} $("#dj_set").hide();
+				$("#dj_set").hide();
+			}
 		}
 	});
 };
